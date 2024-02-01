d05a4e4afa3f9dab0814080a01ffffdec0dfcf91
amitgandhinz <amit@gandhi.co.nz>
2024-01-31 21:15:32 -0500

    made the stock chart appear if one of the results is a stock ticker

--- assets/js/shared/search.js
+++ assets/js/shared/search.js
@@ -1,10 +1,76 @@
 function displayResults(results, store) {
     const searchResults = $('#results');
+    let foundStocks = false;
+
     if (results.length) {
         let cards = '';
-        // Iterate and build card elements
+        var stockWidgetConfig = '';
+
+        // Check if any of the results have a tag of 'stocks'
         for (const n in results) {
             const item = store[results[n].ref];
+            if (item.ticker) {
+                foundStocks = true;
+                // Add only the widget container
+                cards += `
+                    <div class="col-12 pb-3">
+                        <div class="card-body">
+                            <div class="tradingview-widget-container">
+                                <div id="tradingview-widget-container__widget"></div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                // Create the configuration object
+                stockWidgetConfig = {
+                    "symbols": [
+                        [
+                            `${item.ticker}|1D` 
+                        ]
+                      ],
+                      "chartOnly": false,
+                      "width": "100%",
+                      "height": 500,
+                      "locale": "en",
+                      "colorTheme": "light",
+                      "autosize": false,
+                      "showVolume": false,
+                      "showMA": false,
+                      "hideDateRanges": false,
+                      "hideMarketStatus": false,
+                      "hideSymbolLogo": false,
+                      "scalePosition": "right",
+                      "scaleMode": "Normal",
+                      "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+                      "fontSize": "10",
+                      "noTimeScale": false,
+                      "valuesTracking": "1",
+                      "changeMode": "price-and-percent",
+                      "chartType": "area",
+                      "maLineColor": "#2962FF",
+                      "maLineWidth": 1,
+                      "maLength": 9,
+                      "lineWidth": 2,
+                      "lineType": 0,
+                      "dateRanges": [
+                        "1d|1",
+                        "1m|30",
+                        "3m|60",
+                        "12m|1D",
+                        "60m|1W",
+                        "all|1M"
+                      ]
+                };
+
+                break; // Stop checking once we find 'stocks'
+            }
+        }
+
+        // Existing logic to build cards
+        for (const n in results) {
+            const item = store[results[n].ref]; 
+
             // Assuming item has a coverImage property for the image URL
             cards += `
                 <div class="col-lg-6 col-12 pb-3">
@@ -28,6 +94,16 @@ function displayResults(results, store) {
                 </div>`;
         }
         searchResults.html(cards); // Use .html() to replace existing content
+
+        // Dynamically create and append the TradingView script
+        if (foundStocks) {
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js';
+            script.async = true;
+            script.innerHTML = JSON.stringify(stockWidgetConfig);
+            document.getElementById('tradingview-widget-container__widget').appendChild(script);
+        }
     } else {
         searchResults.text('No results found.');
     }
